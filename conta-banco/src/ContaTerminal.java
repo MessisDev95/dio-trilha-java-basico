49c3b2b7f354aa343d1b3fe15b9b65edee65345f
Messias Oliveira <messiasoliveira2005@gmail.com>
2025-07-12 22:17:42 -0300

    feat: create terminal bank account

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,26 @@
-public class App {
+import java.util.Scanner;
+
+public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Bem vindo!");
+        System.out.println("Por favor, digite o número da agencia:");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da conta:");
+           int numeroConta = scanner.nextInt();
+          
+          scanner.nextLine();
+
+        System.out.println("Por favor, digite o nome do cliente:");
+          String nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o saldo da conta:");
+          double saldo = scanner.nextDouble();
+          
+        System.out.println("Olá "+ nomeCliente +", obrigado por criar uma conta em nosso banco.");
+        System.out.println("Sua agência é "+ agencia +", conta "+ numeroConta +" e seu saldo "+ saldo +"R$, já está disponível para saque");  
+      
     }
 }
